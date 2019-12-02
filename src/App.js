d98da02f7079b12906a19fe80c85ab28ec8c38d7
Joshua Lang <langjoshua@icloud.com>
2019-12-02 14:33:13 +0100

    Add useTransition with opacity

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react"
-import { Switch, Route } from "react-router-dom"
-import { useSpring, animated } from "react-spring"
+import { Switch, Route, useLocation } from "react-router-dom"
+import { useSpring, useTransition, animated } from "react-spring"
 
 import Main from "./Main"
 import Header from "./Header"
@@ -15,17 +15,26 @@ const data = mockUpData[0]
 
 function App() {
   const [isMenuShown, setIsMenuShown] = useState(false)
-  const props = useSpring({
-    config: { tension: 5000, mass: 1, friction: 300 },
-    opacity: 1,
-    transform: "translateY(0)",
-    from: { opacity: 0, transform: "translateY(100%)" }
-  })
 
   function onMenuClick() {
     setIsMenuShown(!isMenuShown)
   }
+  //rect-spring trasition
+  const location = useLocation()
+  console.log("location", location)
+  const animationConfig = {
+    config: { tension: 50, mass: 1, friction: 2 },
+    from: { opacity: 0 },
+    enter: { opacity: 1 },
+    leave: { opacity: 0 }
+  }
 
+  const transitions = useTransition(
+    location,
+    location => location.pathname,
+    animationConfig
+  )
+  console.log(transitions)
   return (
     <>
       {isMenuShown ? (
@@ -33,24 +42,29 @@ function App() {
       ) : (
         ""
       )}
-      <Switch>
-        <Route path="/home">
-          <Header onMenuClick={onMenuClick} showTitle="true" />
-          <Main style={props}>
-            <Vaccinations data={data}></Vaccinations>
-          </Main>
-        </Route>
-        <Route path="/addvaccination">
-          <Main style={props} fullscreen="true">
-            <VaccinationForm></VaccinationForm>
-          </Main>
-        </Route>
-        <Route path="/vaccinationdetails/:id">
-          <Main style={props} fullscreen="true">
-            <VaccinationDetails data={data}></VaccinationDetails>
-          </Main>
-        </Route>
-      </Switch>
+      <Header onMenuClick={onMenuClick} showTitle="true" />
+      {transitions.map(({ item, props, key }) => (
+        <animated.div key={key} style={props}>
+          {console.log("item", item, "props", props, "key", key)}
+          <Switch location={item}>
+            <Route path="/home">
+              <Main>
+                <Vaccinations data={data}></Vaccinations>
+              </Main>
+            </Route>
+            <Route path="/addvaccination">
+              <Main fullscreen="true">
+                <VaccinationForm></VaccinationForm>
+              </Main>
+            </Route>
+            <Route path="/vaccinationdetails/:id">
+              <Main fullscreen="true">
+                <VaccinationDetails data={data}></VaccinationDetails>
+              </Main>
+            </Route>
+          </Switch>
+        </animated.div>
+      ))}
     </>
   )
 }
